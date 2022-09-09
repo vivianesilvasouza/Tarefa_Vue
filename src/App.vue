c8bc1ea9e9d0df0e88fc10e0e903581f09de80e0
Viviane Da Silva Souza <vivianedasilvasouza2020@gmail.com>
2022-09-09 18:59:33 -0300

    feat: parte logica para registrar

--- src/App.vue
+++ src/App.vue
@@ -3,23 +3,60 @@
     <div class="column is-one-quarter">
       <BarraLateral />
     </div>
-    <div class="column is-three-quarter"><FormularioTarefa /></div>
+    <div class="column is-three-quarter">
+      <FormularioTarefa @aoSalvarTarefa="salvarTarefa" />
+      <div class="lista">
+        <TarefaDaTarefa
+          v-for="(tarefa, index) in tarefas"
+          :key="index"
+          :tarefa="tarefa"
+        />
+        <BoxDaTarefa v-if="listaEstaVazia"
+          >Você não possui nem uma tarefa registrada!<br />
+          Vamos fazer?</BoxDaTarefa
+        >
+      </div>
+    </div>
   </main>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import BarraLateral from "./components/BarraLateral.vue";
+import BoxDaTarefa from "./components/BoxDaTarefa.vue";
 import FormularioTarefa from "./components/FormularioTarefa.vue";
+import TarefaDaTarefa from "./components/TarefaDaTarefa.vue";
+import ITarefa from "./interfaces/ITarefa";
 
 export default defineComponent({
   name: "App",
   components: {
     BarraLateral,
     FormularioTarefa,
+    TarefaDaTarefa,
+    BoxDaTarefa,
+  },
+
+  data() {
+    return {
+      tarefas: [] as ITarefa[],
+    };
+  },
+  computed: {
+    listaEstaVazia(): boolean {
+      return this.tarefas.length === 0;
+    },
+  },
+  methods: {
+    salvarTarefa(tarefa: ITarefa) {
+      this.tarefas.push(tarefa);
+    },
   },
 });
 </script>
 
 <style>
+.lista {
+  padding: 1.25rem;
+}
 </style>
---
+++ src/components/BoxDaTarefa.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="box has-text-weight-bold"><slot></slot></div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from "vue";
+
+export default defineComponent({
+  name: "BoxDaTarefa",
+});
+</script>
+
+<style scoped>
+.box {
+  background: rgb(236, 235, 235);
+}
+</style>
